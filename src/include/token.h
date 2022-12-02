bd35f0da1f212a89aee57cc01098bfcc4e3fa85a
Shawn <98626576+ShawnAndSean@users.noreply.github.com>
2022-12-02 14:18:12 +0800

    restore lope xd

---
+++ src/include/token.h
@@ -0,0 +1,23 @@
+#ifndef TOKEN_H
+#define TOKEN_H
+
+typedef struct TOKEN_STRUCT{
+    enum{
+        //id is identifiers
+        TOKEN_ID,
+        TOKEN_EQUALS,
+        TOKEN_STRING,
+        TOKEN_SEMI,
+        TOKEN_LPAREN,
+        TOKEN_RPAREN
+    }type;
+
+    char* value;
+
+}token_t;
+
+token_t* init_token(int type, char* value);
+
+
+
+#endif
---
+++ src/token.c
@@ -0,0 +1,15 @@
+#include "include/token.h"
+#include <stdlib.h>
+
+// int is the default parameter if it is an enum from token.h
+token_t* init_token(int type, char* value){
+
+    //allocate a memory for the token 
+    token_t* token = calloc(1,sizeof(struct TOKEN_STRUCT));
+    //whatever the type of the token, assign it as its own
+    token -> type = type;
+    //whatever the character value, assign it as the token value
+    token-> value = value;
+
+    return token; 
+}
