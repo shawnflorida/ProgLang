78f5b63f65399ad6c92aed5f33c5ac82ace14c6f
Yuzyx17_Nablo <braitboat@gmail.com>
2022-12-02 14:11:07 +0800

    Temporary working method for command line input

---
+++ lope/src/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "include/main.h"
+int main(int argc, char **argv)
+{
+
+    fptr = fopen(argv[1], "r");
+    if (fptr != NULL)
+    {
+        readFile();
+        lexer = init_lexer(fileContent);
+        scanFile(lexer);
+    }
+    else
+    {
+        lexer = init_lexer(
+            "linya pangalan = \"shawn michael florida\";"
+            "lahad(pangalan);");
+        scanFile(lexer);
+    }
+    return 0;
+}
+
+int scanFile(lexer_t *lexer)
+{
+
+    token_t *token = (void *)0;
+
+    while ((token = lexer_get_next_token(lexer)) != (void *)0)
+    {
+        printf("TOKEN(%d,%s)\n", token->type, token->value);
+    }
+}
+
+int readFile()
+{
+    fseek(fptr, 0L, SEEK_END);
+    fileLen = ftell(fptr);
+    fseek(fptr, 0L, SEEK_SET);
+
+    fileContent = calloc(fileLen, sizeof(char));
+    fread(fileContent, sizeof(char), fileLen, fptr);
+}
