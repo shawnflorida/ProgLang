4499aece72eed913d04b2cce728d3c5cf704cd94
Yuzyx17_Nablo <braitboat@gmail.com>
2022-12-03 12:26:09 +0800

    Added tokens

--- lope/src/include/token.h
+++ lope/src/include/token.h
@@ -5,20 +5,44 @@ typedef struct TOKEN_STRUCT
 {
     enum
     {
-        // id is identifiers
-        TOKEN_ID,
-        TOKEN_EQUALS,
-        TOKEN_STRING,
-        TOKEN_SEMI,
-        TOKEN_LPAREN,
-        TOKEN_RPAREN,
-        // ADD: other operators
-        TOKEN_ADD,
-        TOKEN_SUB,
-        TOKEN_DIV,
-        TOKEN_MULT,
-        TOKEN_MOD,
-        TOKEN_EXP,
+        /* id is identifiers */
+        TOKEN_ID,       //[a-zA-Z0-9]
+        TOKEN_EQUALS,   // =
+        TOKEN_STRING,   // "[a-!]"
+        TOKEN_SEMI,     // ;
+        TOKEN_LPAREN,   // (
+        TOKEN_RPAREN,   // )
+        TOKEN_LBRACKET, // [
+        TOKEN_RBRACKET, // ]
+        TOKEN_LBRACE,   // {
+        TOKEN_RBRACE,   // }
+        TOKEN_COMMENT,  // #
+        /* ADD: other operators */
+        TOKEN_ADD,     // +
+        TOKEN_SUB,     // -
+        TOKEN_DIV,     // /
+        TOKEN_MULT,    // \*
+        TOKEN_MOD,     // %
+        TOKEN_EXP,     // ^
+        TOKEN_LESS,    // <
+        TOKEN_GREATER, // >
+        TOKEN_NEGATE,  // !
+        /* ADD: keywords */
+        TOKEN_WHLE,     // habang
+        TOKEN_IF,       // kung
+        TOKEN_ELSE,     // sakali
+        TOKEN_ELIF,     // kundi
+        TOKEN_FOR,      // para
+        TOKEN_FUNCTION, // depinisyon/dep
+        TOKEN_IMPORT,   // angkat
+        TOKEN_TRY,      // subukan
+        TOKEN_EXCEPT,   // maliban
+        TOKEN_FINALLY,  // wakas
+        TOKEN_BREAK,    // tigil
+        TOKEN_CONTINUE, // tuloy
+        TOKEN_RETURN,   // balik
+        /* unknown token */
+        TOKEN_UNKNOWN // invalid
     } type;
 
     char *value;
