b192a14aa94a80a502606b589c803365d390b4c9
Yuzyx17_Nablo <braitboat@gmail.com>
2022-12-08 21:19:01 +0800

    Added extension checker and token output

--- lope/src/include/addFile.h
+++
@@ -1,9 +0,0 @@
-#ifndef ADDFILE_H
-#define ADDFILE_H
-
-#include "lexer.h"
-
-int *find_file();
-int scanFile(lexer_t *lexer);
-
-#endif
--- lope/src/main.c
+++ lope/src/main.c
@@ -1,30 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "include/main.h"
-#include "include/addFile.h"
-#include "include/lexer.h"
+#include "include/fileHandler.h"
 
 int main(int argc, char **argv)
 {
     for (int i = 1; i < argc; i++)
     {
-        fptr = fopen(argv[i], "r");
-        if (fptr != NULL)
+        input_ptr = fopen(argv[i], "r");
+        if (input_ptr != NULL && check_Extension(argv[i]) == 1)
         {
             find_file();
             lexer = init_lexer(fileContent);
+            create_TokenOutput(argv[i]);
             scanFile(lexer);
         }
         else
         {
-            lexer = init_lexer(
-                "linya error = \"may problema\";");
+            printf("Error on reading argument %s\n", argv[i]);
+            // lexer = init_lexer(
+            //     "linya error = \"may problema\";");
 
-            // numero a = 1;
-            scanFile(lexer);
+            // // numero a = 1;
+            // scanFile(lexer);
         }
     }
-    free(fptr);
+    fclose(input_ptr);
+    fclose(output_ptr);
     free(fileContent);
     return 0;
 }
